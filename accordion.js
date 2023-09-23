db82fa32f6ce79dd63ce8e6fa4c6cbb2637689a0
hikaruminagawa <96165184+hikaruminagawa@users.noreply.github.com>
2023-09-23 16:39:44 +0900

    1st commit

---
+++ accordion.js
@@ -0,0 +1,26 @@
+(()=>{
+    const $elm = document.querySelector('#js-accordion');
+    const $trigger = $elm.getElementsByTagName('a');
+
+    const triggerLen = $trigger.length;
+    
+
+    const clickHandler = (e)=> {
+        e.preventDefault();
+        const $target = e.currentTarget;
+        const $content = $target.nextElementSibling;
+        if($content.style.display ==='block') {
+            $content.style.display = 'none'
+        } else {
+            $content.style.display ='block'
+        }
+    };
+
+    let index = 0;
+    while(index<triggerLen){
+        $trigger[index].addEventListener('click',clickHandler);
+        index++;
+    }
+
+
+})()
